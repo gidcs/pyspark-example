2c975a2b8ff85da92e77a31c50ae5f2599b1a4c1
Kok Suan Lim <admin@gidcs.net>
2017-01-05 01:44:19 +0800

    add sshversion example

--- README.md
+++ README.md
@@ -9,3 +9,12 @@ wordcount
 	hadoop fs -put data/word.txt word.txt # put new txt file
 	spark-submit --master yarn --deploy-mode client wordcount.py word.txt output # run application
 	hadoop fs -cat output/* #show results
+
+sshversion
+===
+
+	hadoop fs -rm -r output # remove output folder
+	hadoop fs -rm ssh.json # remove old json file
+	hadoop fs -put data/ssh.json ssh.json # put new json file
+	spark-submit --master yarn --deploy-mode client sshversion.py ssh.json output # run application
+	hadoop fs -cat output/* #show results
---
+++ sshversion.py
@@ -0,0 +1,50 @@
+# get parameter
+import sys
+# spark
+from pyspark import SparkContext, SparkConf
+# json object
+import json
+
+def parse_json(line):
+    data = json.loads(line)
+    server_protocol = {}
+    if 'data' in data:
+        if 'ssh' in data['data']:
+            if 'server_protocol' in data['data']['ssh']:
+                server_protocol = data['data']['ssh']['server_protocol']
+    ret = ""
+    if 'raw_banner' in server_protocol:
+        raw_banner = server_protocol['raw_banner'].rstrip()
+        if '220 ' in raw_banner:
+            ret = "FTP"
+        elif 'HTTP/' in raw_banner:
+            ret = "HTTP"
+        else:
+            ret = raw_banner
+    else:
+        ret = "invalid"
+    
+    return [ret]
+
+if __name__ == "__main__":
+
+    inputJson = sys.argv[1]
+    outputDir = sys.argv[2]
+
+    # create Spark context with Spark configuration
+    conf = SparkConf().setAppName("SSHVersion")
+    sc = SparkContext(conf=conf)
+
+    # read in text file and split each document into words
+    text_file = sc.textFile(inputJson)
+    words = text_file.flatMap(parse_json)
+
+    # count the occurrence of each word
+    counts = words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+
+    result = counts.collect()
+
+    # for (word, count) in result:
+    #    print("%s: %i" % (word, count))
+
+    counts.saveAsTextFile(outputDir)
